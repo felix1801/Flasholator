eab8ef389108b39aed1d20e932f75486926a79e9
felix1801 <noiset.lapin@hotmail.fr>
2024-08-18 18:36:27 +0200

    Add more languages

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,9 @@ const String TARGET_LANGUAGE = 'EN';
 const Map<String, String> LANGUAGES = {
   'FR': 'Français',
   'ES': 'Español',
-  'EN': 'English'
+  'EN': 'English',
+  'IT': 'Italiano',
+  'DE': 'Deutsch',
+  'PT': 'Português',
   // Add more languages as needed
 };
